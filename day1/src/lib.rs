7f0df88cd8d272bfd048dd1c1138c1b697f1445b
John Oxley <john.oxley@gmail.com>
2022-12-03 19:47:59 +0100

    day1 part 2

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -9,7 +9,13 @@ pub fn day1_part1() {
     let file_path = "../day1/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     match parser::parse_whole_file(&contents) {
-        Ok((_, elves)) => println!("Most calorific elf: {:?}", elves.iter().max().unwrap()),
+        Ok((_, mut elves)) => {
+            elves.sort();
+            elves.reverse();
+            println!("Most calorific elf: {:?}", elves.iter().max().unwrap());
+            let top3: i64 = elves.iter().take(3).sum();
+            println!("Top 3 elves calories: {:}", top3);
+        }
         Err(e) => eprintln!("{:?}", e),
     };
 }
