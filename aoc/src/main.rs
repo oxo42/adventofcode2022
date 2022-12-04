28db6917d3f2f9cf5feb9ae6bb1d62f2dcacc635
John Oxley <john.oxley@gmail.com>
2022-12-04 20:52:12 +0100

    Day 4 start

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,12 +1,12 @@
 use std::time::Instant;
 
-fn main() -> eyre::Result<()> {
+fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     color_backtrace::install();
 
     let start = Instant::now();
     // day1::day1_part1();
-    day3::run()?;
+    day4::run()?;
     let duration = Instant::now().duration_since(start);
     println!("It took {:?}", duration);
     Ok(())
