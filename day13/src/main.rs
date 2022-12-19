4ae8778ef772c7b6e55f3421fd9dafae3fb4bbd9
John Oxley <john.oxley@gmail.com>
2022-12-19 14:01:04 +0200

    Day 13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,27 +1,27 @@
 #![allow(unused)]
 
-use std::{collections::VecDeque, fmt::Debug};
+use std::{cmp::Ordering, collections::VecDeque, fmt::Debug};
 
-use crate::parser::parse_input;
+use itertools::Itertools;
+use serde::Deserialize;
 
-mod parser;
-
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone)]
-enum Item {
-    Collection(VecDeque<Item>),
+#[derive(PartialEq, Eq, Clone, Deserialize)]
+#[serde(untagged)]
+enum Node {
+    Collection(Vec<Node>),
     Value(i64),
 }
 
-// impl Item {
-//     fn with_slice<T>(&self, f: impl FnOnce(&[Item]) -> T) -> T {
-//         match self {
-//             Self::Collection(n) => f(&n[..]),
-//             Self::Value(n) => f(&[Self::Value(*n)]),
-//         }
-//     }
-// }
+impl Node {
+    fn with_slice<T>(&self, f: impl FnOnce(&[Node]) -> T) -> T {
+        match self {
+            Self::Collection(n) => f(&n[..]),
+            Self::Value(n) => f(&[Self::Value(*n)]),
+        }
+    }
+}
 
-impl Debug for Item {
+impl Debug for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Collection(arg0) => f.debug_list().entries(arg0.iter()).finish(),
@@ -30,130 +30,69 @@ impl Debug for Item {
     }
 }
 
-impl From<Vec<i64>> for Item {
-    fn from(v: Vec<i64>) -> Self {
-        Item::Collection(v.into_iter().map(Into::into).collect())
-    }
-}
-
-impl From<VecDeque<Item>> for Item {
-    fn from(v: VecDeque<Item>) -> Self {
-        Item::Collection(v)
-    }
-}
-
-impl From<i64> for Item {
-    fn from(i: i64) -> Self {
-        Item::Value(i)
-    }
-}
-
-impl From<Vec<Item>> for Item {
-    fn from(v: Vec<Item>) -> Self {
-        Item::Collection(VecDeque::from(v))
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Pair(Item, Item);
-
-impl From<(Item, Item)> for Pair {
-    fn from((left, right): (Item, Item)) -> Self {
-        Pair(left, right)
-    }
-}
-
-impl From<(VecDeque<Item>, VecDeque<Item>)> for Pair {
-    fn from((left, right): (VecDeque<Item>, VecDeque<Item>)) -> Self {
-        Pair(Item::Collection(left), Item::Collection(right))
-    }
-}
-
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    let input = include_str!("input.txt");
-    let pairs = parse_input(input)?;
-    println!("Got {} pairs", pairs.len());
-
-    let corrects = sum_corrects(pairs);
-    println!("Corrects: {}", corrects);
-    Ok(())
-}
-
-fn icp(Pair(left, right): Pair) -> bool {
-    use Item::*;
-    match (left, right) {
-        (Collection(mut l_collection), Collection(mut r_collection)) => {
-            let l_head = if let Some(l_head) = l_collection.pop_front() {
-                l_head
-            } else {
-                return true;
-            };
-            let r_head = if let Some(r_head) = r_collection.pop_front() {
-                r_head
-            } else {
-                return false;
-            };
-            match (l_head, r_head) {
-                (Value(ll), Value(rr)) => {
-                    if ll < rr {
-                        return true;
-                    }
-                    if ll > rr {
-                        return false;
-                    }
-                    // if ll == rr
-                    icp(Pair::from((l_collection, r_collection)))
-                }
-                (ll, rr) => icp(Pair(ll, rr)),
-            }
+impl std::cmp::PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        use Node::*;
+        match (self, other) {
+            (Value(a), Value(b)) => a.partial_cmp(b),
+            (l, r) => Some(l.with_slice(|l| {
+                r.with_slice(|r| {
+                    l.iter()
+                        .zip(r.iter())
+                        .map(|(aa, bb)| aa.cmp(bb))
+                        .find(|&ord| ord != Ordering::Equal)
+                        .unwrap_or_else(|| l.len().cmp(&r.len()))
+                })
+            })),
         }
-        (Collection(l), Value(r)) => icp(Pair::from((Collection(l), vec![r].into()))),
-        (Value(l), Collection(r)) => icp(Pair::from((vec![l].into(), Collection(r)))),
-        _ => unreachable!(),
     }
 }
 
-fn sum_corrects(pairs: Vec<Pair>) -> usize {
-    pairs
-        .into_iter()
+impl std::cmp::Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+fn part1() {
+    let input = include_str!("input.txt");
+    let sum: usize = input
+        .split("\n\n")
         .enumerate()
-        .map(|(i, p)| match icp(p) {
-            true => (i + 1),
-            false => 0,
+        .map(|(i, groups)| {
+            let (l, r) = groups
+                .lines()
+                .map(|line| serde_json::from_str::<Node>(line).unwrap())
+                .collect_tuple()
+                .unwrap();
+            if l < r {
+                i + 1
+            } else {
+                0
+            }
         })
-        .sum()
+        .sum();
+    println!("Sum is {sum}");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test_case::test_case;
-    use Item::*;
+fn main() {
+    let input = include_str!("input.txt");
+    let dividers = vec![
+        Node::Collection(vec![Node::Value(2)]),
+        Node::Collection(vec![Node::Value(6)]),
+    ];
 
-    #[test_case("[1,1,3,1,1]\n[1,1,5,1,1]\n")]
-    #[test_case("[]\n[3]\n")]
-    #[test_case("[[1],[2,3,4]]\n[[1],4]\n")]
-    #[test_case("[[4,4],4,4]\n[[4,4],4,4,4]\n")]
-    fn test_correct_order(input: &str) {
-        let pair: Pair = parse_input(input).unwrap().first().unwrap().clone();
-        assert!(icp(pair));
-    }
+    let mut packets = input
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|line| serde_json::from_str::<Node>(line).unwrap())
+        .chain(dividers.iter().cloned())
+        .collect_vec();
+    packets.sort();
 
-    #[test_case("[9]\n[[8,7,6]]\n")]
-    #[test_case("[[[]]]\n[[]]\n")]
-    #[test_case("[7,7,7,7]\n[7,7,7]\n")]
-    #[test_case("[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]\n")]
-    fn test_incorrect_order(input: &str) {
-        let pair: Pair = parse_input(input).unwrap().first().unwrap().clone();
-        assert!(!icp(pair));
-    }
-
-    #[test]
-    fn test_sample_part_1() -> color_eyre::Result<()> {
-        let pairs = parse_input(include_str!("sample.txt"))?;
-        let actual = sum_corrects(pairs);
-        assert_eq!(13, actual);
-        Ok(())
-    }
+    let decoder_key: usize = dividers
+        .iter()
+        .map(|d| packets.binary_search(d).unwrap() + 1)
+        .product();
+    dbg!(decoder_key);
 }
